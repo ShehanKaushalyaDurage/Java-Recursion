de342939a111e3d4a7b3eadd16581d3cd3308178
shehan kaushalya <shehankaushalyadurage@gmail.com>
2025-12-19 15:20:26 +0530

    Commit changes

---
+++ src/main/java/com/shehan/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.shehan;
+
+public class Fibonacci {
+    static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return fibonacci( n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 7; i++) {
+            System.out.println(fibonacci(i) + " ");
+        }
+    }
+}
+

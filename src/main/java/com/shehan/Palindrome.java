41754ce7e418bd562075fe41992dbc9cbb8cc390
shehan kaushalya <shehankaushalyadurage@gmail.com>
2025-12-19 15:57:47 +0530

    Commit changes

--- src/main/java/com/shehan/Palindrome.java
+++ src/main/java/com/shehan/Palindrome.java
@@ -1,4 +1,19 @@
 package com.shehan;
 
 public class Palindrome {
+    static boolean isPalindrome(String s, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(s, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("madam", 0, 4));
+    }
 }
